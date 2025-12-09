bc7acfdb855302bb64c19b03e78aadaf2c5ad3cc
gobbls <98641699+gobbls@users.noreply.github.com>
2025-12-09 22:02:21 +0100

    testing: added (a damn slow, single threaded) solution for day07 p1
    rewriting for multithreading.

---
+++ day-07/p2/Solution.cs
@@ -0,0 +1,37 @@
+using System.Linq;
+using aoclib;
+
+namespace day_07_p2;
+
+public class Solution {
+	public List<string> Input { get; private set; }
+	public long Output { get; private set; } = 0;
+
+	private const char C = '^';
+	private const char S = 'S';
+
+	public Solution(bool? isTest) {
+		Input = ReadInput.GetInput(isTest: isTest).ToList();
+		run();
+	}
+
+	private void run() {
+		void traverse(int x, int y) {
+			Console.Write($"[DEBUG] x = {x} y = {y}, Output = {Output}\r");
+
+			if (y == Input.Count - 1) {
+				Output++;
+				return;
+			} else {
+				if (Input[y][x] == C) {
+					traverse(x - 1, y + 1);
+					traverse(x + 1, y + 1);
+				} else {
+					traverse(x, y + 1);
+				}
+			}
+		}
+
+		traverse(Input[0].IndexOf(S), 1);
+	}
+}
