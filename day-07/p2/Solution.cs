3847cebdec2725341186259f9f335156b60a1920
gobbls <98641699+gobbls@users.noreply.github.com>
2025-12-09 22:33:36 +0100

    chore: converted list to array for maybe some perf imporvments

--- day-07/p2/Solution.cs
+++ day-07/p2/Solution.cs
@@ -4,22 +4,22 @@ using aoclib;
 namespace day_07_p2;
 
 public class Solution {
-	public List<string> Input { get; private set; }
+	public string[] Input { get; private set; }
 	public long Output { get; private set; } = 0;
 
 	private const char C = '^';
 	private const char S = 'S';
 
 	public Solution(bool? isTest) {
-		Input = ReadInput.GetInput(isTest: isTest).ToList();
+		Input = ReadInput.GetInput(isTest: isTest);
 		run();
 	}
 
 	private void run() {
 		void traverse(int x, int y) {
-			Console.Write($"[DEBUG] x = {x} y = {y}, Output = {Output}\r");
+			//Console.WriteLine($"[DEBUG] x = {x} y = {y}, Output = {Output}\r");
 
-			if (y == Input.Count - 1) {
+			if (y == Input.Length - 1) {
 				Output++;
 				return;
 			} else {
